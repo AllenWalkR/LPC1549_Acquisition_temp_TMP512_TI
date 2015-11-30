5657b24b90fde860beaef9e384e125bd391604e9
AllenWalkR <cochetjon@gmail.com>
2015-11-30 23:19:53 +0100

    I2C
    Ajout de la modification pour le fonctionnement du i2c

---
+++ Partie Arduino/Master_Reader_I2C/Master_Reader_I2C.ino
@@ -0,0 +1,34 @@
+// Wire Master Reader
+// by Nicholas Zambetti <http://www.zambetti.com>
+
+// Demonstrates use of the Wire library
+// Reads data from an I2C/TWI slave device
+// Refer to the "Wire Slave Sender" example for use with this
+
+// Created 29 March 2006
+
+// This example code is in the public domain.
+
+
+#include <Wire.h>
+
+void setup()
+{
+  Wire.begin();        // join i2c bus (address optional for master)
+  Serial.begin(9600);  // start serial for output
+}
+
+void loop()
+{
+  Wire.requestFrom(0x08, 3);    // request 6 bytes from slave device #8
+  Wire.beginTransmission(8); // transmit to device #8
+  Wire.write("68");              // sends one byte
+  Wire.endTransmission();    // stop transmitting
+  while (Wire.available())   // slave may send less than requested
+  {
+    char c = Wire.read(); // receive a byte as character
+    Serial.print(c);         // print the character
+  }
+
+  delay(500);
+}
